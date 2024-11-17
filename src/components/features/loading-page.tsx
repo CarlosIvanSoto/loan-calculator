b8c51847f8a923c079e3c75cf42ec16078e5971d
Carlos Ivan Soto Perez <gameue@hotmail.com>
2024-11-16 18:48:03 -0600

    fix: build error on vercel correctons

--- src/components/features/loading-page.tsx
+++ src/components/features/loading-page.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Phone, Printer, Scissors, ShoppingBag, ImageIcon, Mail, MessageCircleHeart, MessageCircle } from 'lucide-react'
+import { Phone, Printer, ShoppingBag, ImageIcon, Mail, MessageCircle } from 'lucide-react'
 
 export default function LoadingPage() {
   return (
